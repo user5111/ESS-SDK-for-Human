6c941a79cf1cdee43d5abd3e626a334be326931e
Wang Wentao <wojiaowwt@gmail.com>
2017-08-08 15:32:54 +0800

    test for speed

--- departinfo.py
+++ departinfo.py
@@ -130,11 +130,11 @@ def getSalesDetailsByDate(begin_date, end_date, depart_id, session, urls):
                     sales_detail['客户姓名'] = custinfos['cust_name']
                     sales_detail['证件地址'] = custinfos['cust_address']
                     sales_details.append(sales_detail)
-                    print(sales_detail)
+                    #print(sales_detail['用户号码'])
             except:
                     sales_detail['客户姓名'] = 'NO CUST INFO FOR THIS NUMBER'
                     sales_details.append(sales_detail)
-                    print(sales_detail)
+                    #print(sales_detail['用户号码'])
     return sales_details
 
 
@@ -144,9 +144,10 @@ if __name__ == '__main__':
         USERNAME = 'zhangqian77'  
         DEPARTID = '11a0271'
         PASSWORD = 'et3YzxYGJfhigS2oi+dh/5J/3WU='
-        depart_ids = ['11b2821','11b27g2','11b22kt','11b1xhs','11b1web','11b1wc7','11b1wc5','11b1ska','11b1sk9','11b1scq','11b1p97','11b1iho','11b1g9r','11b1fmw','11b1dzj','11b1dz7','11b1dz5','11b1cul','11b13ds','11b0y2o','11b0xns','11b0wqy','11b0jl9','11b0d0w','11b0cir','11b0ayi','11b0ag5','11b082z','11b03ye','11a2570','11b0bon','11b13rr']
-        begin_date = '2017-07-01'
-        end_date = '2017-08-06'
+        #depart_ids = ['11b2821','11b27g2','11b22kt','11b1xhs','11b1web','11b1wc7','11b1wc5','11b1ska','11b1sk9','11b1scq','11b1p97','11b1iho','11b1g9r','11b1fmw','11b1dzj','11b1dz7','11b1dz5','11b1cul','11b13ds','11b0y2o','11b0xns','11b0wqy','11b0jl9','11b0d0w','11b0cir','11b0ayi','11b0ag5','11b082z','11b03ye','11a2570','11b0bon','11b13rr']
+        depart_ids = ['11b2821','11b27g2','11b22kt']
+        begin_date = '2017-06-15'
+        end_date = '2017-07-01'
         wb = Workbook()
         dest_filename = '/Users/wangwentao/Desktop/test.xlsx'
         ws1 = wb.active
--- test.py
+++ test.py
@@ -16,8 +16,8 @@ USERNAME = 'zhangqian77'
 DEPARTID = '11a0271'
 PASSWORD = 'et3YzxYGJfhigS2oi+dh/5J/3WU='
 #depart_ids = ['11b2821','11b27g2','11b22kt','11b1xhs','11b1web','11b1wc7','11b1wc5','11b1ska','11b1sk9','11b1scq','11b1p97','11b1iho','11b1g9r','11b1fmw','11b1dzj','11b1dz7','11b1dz5','11b1cul','11b13ds','11b0y2o','11b0xns','11b0wqy','11b0jl9','11b0d0w','11b0cir','11b0ayi','11b0ag5','11b082z','11b03ye','11a2570','11b0bon','11b13rr']
-depart_ids = ['11b2821','11b1scq']
-begin_date = '2017-08-03'
+depart_ids = ['11b2821','11b27g2','11b22kt']
+begin_date = '2017-08-01'
 end_date = '2017-08-06'
 wb = Workbook()
 dest_filename = '/Users/wangwentao/Desktop/test.xlsx'
@@ -30,7 +30,10 @@ session = requests.Session()
 urls = login.LoginEssSystem(USERNAME,DEPARTID,PASSWORD,session)
 login.LoginService(urls, 'ESS业务受理明细查询','stat', session)
 for depart_id in depart_ids:
+        begin = datetime.now()
         sales_details = departinfo.getSalesDetailsByDate(begin_date, end_date, depart_id, session, urls)
+        end = datetime.now()
+        print('run time: '+ str(end - begin))
         for sales_detail in sales_details:
             ws1.append(list(value for (key,value) in sales_detail.items()))
 wb.save(filename = dest_filename)
